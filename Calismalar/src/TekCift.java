3cda47ec5de7fcb5d1f2dbde55e158310d4fb9a1
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-25 00:44:46 +0300

    5. ödev

---
+++ Calismalar/src/TekCift.java
@@ -0,0 +1,13 @@
+public class TekCift {
+public static void tekCiftSayilar(int a){
+    if (a%2==0){
+        System.out.println("cift bir sayıdır");
+    }else{
+        System.out.println("tek bir sayıdır");
+    }
+}
+
+    public static void main( String[] args ) {
+        tekCiftSayilar(54);
+    }
+}
