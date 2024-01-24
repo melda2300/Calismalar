d0973c2eb7460014b06939b5bacb1404d28179d8
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-24 17:07:51 +0300

    1. ödev

---
+++ Calismalar/src/beginning.java
@@ -0,0 +1,8 @@
+public class beginning {
+
+
+    public static void main( String[] args ) {
+ // Soru 1: Bir Java programında ekrana "Merhaba, Dünya!" yazdırmak için hangi kodu kullanırsınız?
+        System.out.println("Hello World"); // cıktı alabilmek için
+    }
+}
