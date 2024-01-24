ddc13ad24270df7386498084faaff2674202865d
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-24 17:10:35 +0300

    2. ödev

---
+++ Calismalar/src/karesi.java
@@ -0,0 +1,13 @@
+public class karesi {
+
+ public static void karesii(int a){
+     System.out.println(a*a);
+
+ }
+
+
+
+    public static void main( String[] args ) {
+        karesii(5);
+    }
+}
